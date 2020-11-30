9280f3b6c4af52bd4610681fc3e17a2531d4d9e2
Summer T <summer@summertime-development.com>
2020-11-30 12:22:24 -0600

    axios call works

--- src/redux/pictureActions.ts
+++ src/redux/pictureActions.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-shadow */
 import { call, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
 
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
@@ -12,6 +13,32 @@ export function* asyncGetPictures(): any {
     try {
         // eslint-disable-next-line no-console
         console.log('asyncGetPictures()');
+
+        const headerParams = {
+            Authorization: `563492ad6f9170000100000196cbe16d24264bd39f0ea60e3f1faa0b`,
+        };
+
+        const apiCall = () => {
+            return axios
+                .get('https://api.pexels.com/v1/curated?per_page=11&page=1', {
+                    headers: headerParams,
+                })
+                .then((response: { data: any }) => response.data)
+                .catch((err: any) => {
+                    throw err;
+                });
+        };
+
+        // const res = await fetch(request);
+        // const { photos } = await res.json();
+
+        const user = yield call(apiCall);
+        // eslint-disable-next-line no-console
+        console.log(
+            '🚀 ~ file: pictureActions.ts ~ line 36 ~ function*asyncGetPictures ~ user',
+            user
+        );
+
         // eslint-disable-next-line no-console
         // const user = yield call(Api.fetchUser, action.payload.userId);
         yield delay(2000);
